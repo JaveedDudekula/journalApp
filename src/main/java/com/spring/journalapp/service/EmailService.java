a6fffda3b02b5b8364b2282e9d91fcc5bd8f2c1b
JaveedDudekula <javeeddudekula@gmail.com>
2025-04-08 22:55:37 +0530

    added email service

---
+++ src/main/java/com/spring/journalapp/service/EmailService.java
@@ -0,0 +1,33 @@
+package com.spring.journalapp.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private JavaMailSender javaMailSender;
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+        } catch (Exception e) {
+            logger.error("Exception occurred while sendEmail: ", e);
+        }
+    }
+}
---
+++ src/test/java/com/spring/journalapp/service/EmailServiceTests.java
@@ -0,0 +1,21 @@
+package com.spring.journalapp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    private EmailService emailService;
+
+    @Autowired
+    public EmailServiceTests(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @Test
+    void testSendMail(){
+        emailService.sendMail("javeeddudekula69@gmail.com","Testing java email service","Hi,\nMessage received.");
+    }
+}
