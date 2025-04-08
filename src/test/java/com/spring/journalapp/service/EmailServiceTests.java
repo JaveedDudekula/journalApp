1ba1bc56bc8784cd347ebe7cf26758473c0d9dd9
JaveedDudekula <javeeddudekula@gmail.com>
2025-04-08 23:08:04 +0530

    added few changes to tests

--- src/test/java/com/spring/journalapp/service/EmailServiceTests.java
+++ src/test/java/com/spring/journalapp/service/EmailServiceTests.java
@@ -1,10 +1,12 @@
 package com.spring.journalapp.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 public class EmailServiceTests {
 
     private EmailService emailService;
